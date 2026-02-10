25ebb73ba76cb2db54b6167fc5b24db2edd4f518
ル行 <yai55860@gmail.com>
2026-02-10 23:06:11 +0900

    Delete scraper directory

--- scraper/utils.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def sanitize_filename(name):
-    return re.sub(r'[\\/:*?"<>|#\s]+', '_', name).rstrip('_')
-
-def shorten_title(title, limit=40):
-    return title[:limit] + ("…" if len(title) > limit else "")
